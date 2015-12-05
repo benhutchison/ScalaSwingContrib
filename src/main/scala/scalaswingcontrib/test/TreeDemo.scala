dfedc1f3a9eee232775ce804cd351263875569f9
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2015-12-05 14:21:16 +0100

    Fixed inspections as suggested by IDEA plugin.

--- src/main/scala/scalaswingcontrib/test/TreeDemo.scala
+++ src/main/scala/scalaswingcontrib/test/TreeDemo.scala
@@ -67,9 +67,17 @@ object TreeDemo extends SimpleSwingApplication {
   lazy val infiniteTree = new Tree(TreeModel(1000) {n => 1 to n filter (n % _ == 0)}) {
     expandRow(0)
   }
-  
-  
-  
+
+  val externalTreeStatusBar = new Label {
+    preferredSize = (100,12)
+  }
+
+  val internalTreeStatusBar = new Label {
+    preferredSize = (100,12)
+  }
+
+
+
   // Use case 5: Mutable external tree model
   val mutableExternalTree = new Tree[PretendFile] {
 
@@ -181,14 +189,6 @@ object TreeDemo extends SimpleSwingApplication {
   }
 
   
-  val externalTreeStatusBar = new Label {
-    preferredSize = (100,12)
-  }
-  
-  val internalTreeStatusBar = new Label {
-    preferredSize = (100,12)
-  }
-  
   // Other setup stuff
   
    
@@ -260,7 +260,7 @@ object TreeDemo extends SimpleSwingApplication {
       childFiles foreach {_.parent = Some(this)}
       private var childBuffer = mutable.ListBuffer(childFiles: _*)
       
-      override def toString() = name
+      override def toString = name
       def name = nameVar
       def rename(str: String): Boolean = if (siblingExists(str)) false 
                                          else { nameVar = str; true }
@@ -274,11 +274,11 @@ object TreeDemo extends SimpleSwingApplication {
           true 
         }
       }
-      def delete(): Boolean = parent.map(_ removeChild this) getOrElse false
+      def delete(): Boolean = parent.exists(_ removeChild this)
       def removeChild(child: PretendFile): Boolean = if (children contains child) {childBuffer -= child; true}
                                                      else false
                                                      
-      def siblingExists(siblingName: String) = parent.map(_ childExists siblingName) getOrElse false
+      def siblingExists(siblingName: String) = parent.exists(_ childExists siblingName)
       def childExists(childName: String) = children.exists(_.name == childName)
       def children: Seq[PretendFile] = childBuffer
       def isDirectory = children.nonEmpty
